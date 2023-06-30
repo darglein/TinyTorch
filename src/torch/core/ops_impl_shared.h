f7a0383df3c3f0ab3faf39eb29183fe5ffad90b6
Philipp Kurth <philipp.kurth@fau.de>
2023-06-30 15:23:58 +0200

    Pulled some ops into shared file for use in cuda

---
+++ src/torch/core/ops_impl_shared.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "torch/tiny_torch_config.h"
+
+namespace TINY_TORCH_NAMESPACE
+{
+template <typename T>
+inline T relu(T x)
+{
+    return (x > T(0)) ? x : T(0);
+}
+
+template <typename T>
+inline T sigmoid(T x)
+{
+    return T(1) / (T(1) + ::exp(-x));
+}
+
+template <typename T>
+inline T softplus(T x, T beta)
+{
+    return T(::log(1 + ::exp(beta * x)) / beta);
+}
+
+
+
+
+template <typename T>
+inline std::pair<T, T> div_backward(T a, T b)
+{
+    return {T(1) / b, -a / (b * b)};
+}
+
+template <typename T>
+inline T log_backward(T x)
+{
+    return T(1) / x;
+}
+
+template <typename T>
+inline T log1p_backward(T x)
+{
+    return T(1) / (x + T(1));
+}
+
+template <typename T>
+inline T pow_backward(T x, T b)
+{
+    return b * ::pow(x, b - 1);
+}
+
+template <typename T>
+inline T sin_backward(T x)
+{
+    return ::cos(x);
+}
+
+template <typename T>
+inline T cos_backward(T x)
+{
+    return -::sin(x);
+}
+
+template <typename T>
+inline T relu_backward(T x)
+{
+    return ((x < T(0)) ? T(0) : T(1));
+}
+
+template <typename T>
+inline T sigmoid_backward(T x)
+{
+    T expnegx = T(::exp(-x));
+    return expnegx / ((T(1) + expnegx) * (T(1) + expnegx));
+}
+
+template <typename T>
+inline T softplus_backward(T x, T beta)
+{
+    T e = T(::exp(beta * x));
+    return e / (e + T(1));
+}
+
+
+
+
+
+
+
+
+}  // namespace TINY_TORCH_NAMESPACE
