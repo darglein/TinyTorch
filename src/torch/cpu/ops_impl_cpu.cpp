a9c9813dd0f5133990d0a1d4901b1b37e2d8df3b
Darius Rueckert <darius.rueckert@fau.de>
2024-04-18 13:40:14 +0200

    uint16 copy

--- src/torch/cpu/ops_impl_cpu.cpp
+++ src/torch/cpu/ops_impl_cpu.cpp
@@ -627,6 +627,11 @@ void copy_and_convert_impl(Tensor src, Tensor& target)
             SWITCH_MACRO_ALL_DUAL(src.scalar_type(), uint8_t, copy_and_convert_impl_kernel, src, target);
             break;
         }
+        case kUInt16:
+        {
+            SWITCH_MACRO_ALL_DUAL(src.scalar_type(), uint16_t, copy_and_convert_impl_kernel, src, target);
+            break;
+        }
         case kInt16:
         {
             SWITCH_MACRO_ALL_DUAL(src.scalar_type(), int16_t, copy_and_convert_impl_kernel, src, target);
